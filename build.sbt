deed6b569b173764b4199ffc674db0c837efc716
Josh Cough <joshcough@gmail.com>
2013-08-13 11:06:46 -0400

    fixed small build errors, published 1.1

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ organization := "com.clarifi"
 
 description := "Multi-language serialization protocol."
 
-version := "1.0.1"
+version := "1.1"
 
 scalaVersion := "2.10.2"
 
--- src/main/scala/f0/Writers.scala
+++ src/main/scala/f0/Writers.scala
@@ -144,8 +144,8 @@ object Writers extends SumWriters {
     def bind(o: Sink) = {
       val bindA = w.bind(o)
       (as) => {
-        as.foreach(a => { o.writeByte(1); bindA(a) })
-        o.writeByte(0)
+        as.foreach(a => { o(1); bindA(a) })
+        o(0)
         effectW[StreamF[F1]]
       }
     }
@@ -159,11 +159,11 @@ object Writers extends SumWriters {
         var cont = true
         while (cont) {
           f(cur) match {
-            case Some((h,t)) => { o.writeByte(1); bindA(h); cur = t }
+            case Some((h,t)) => { o(1); bindA(h); cur = t }
             case None => { cont = false }
           }
         }
-        o.writeByte(0)
+        o(0)
         effectW[StreamF[F1]]
       }
     }
